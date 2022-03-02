946ded0867d58bf026d746c7bdaf7de99406b003
mickefalt <76034473+mickefalt@users.noreply.github.com>
2022-03-02 09:47:43 +0100

    initial commit

---
+++ hejhej-app-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hejhej_app_tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    Assert.IsTrue(true);
+    }
+}
